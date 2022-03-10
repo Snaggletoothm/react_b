3f7beef4ae5eeca3d4ebc9daceee55b39fe6012e
Mikriukov MA <snagglet00th@yandex.ru>
2022-03-10 21:47:02 +0500

    add element 'hello world' via React.createElement

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+// const el = <h1>Hello World!!!</h1>
+
+const el = React.createElement(
+  'h1',
+  null,
+  'hello'
+);
+
+ReactDOM.render(el, document.getElementById('root'));
