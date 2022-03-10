3679991016e495ecd46a765285354eabd83e6323
Mikriukov MA <snagglet00th@yandex.ru>
2022-03-10 22:04:44 +0500

    incapsulate each elem into custom react component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-const el = (
-    <div>
-        <h1>My Todo List</h1>
-        <input placeholder='search' />
+const AppHeader = () => <h1>My Todo List</h1>;
+
+
+const SearchPanel = () => {
+    return <input placeholder='search' />
+};
+
+
+const TodoList = () => {
+    return (
         <ul>
             <li>Learn React</li>
             <li>Build Awesome App</li>
         </ul>
+    );
+};
+
+const el = (
+    <div>
+        <AppHeader />        
+        <SearchPanel />
+        <TodoList />
     </div>
 );
 
