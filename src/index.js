aa684ac428b2e8f4662bfc537e9e56fd7a1f61c6
Mikriukov MA <snagglet00th@yandex.ru>
2022-03-10 21:52:21 +0500

    create search form with list items(simple implementation)

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-// const el = <h1>Hello World!!!</h1>
-
-const el = React.createElement(
-  'h1',
-  null,
-  'hello'
+const el = (
+    <div>
+        <h1>My Todo List</h1>
+        <input placeholder='search' />
+        <ul>
+            <li>Learn React</li>
+            <li>Build Awesome App</li>
+        </ul>
+    </div>
 );
 
-ReactDOM.render(el, document.getElementById('root'));
+ReactDOM.render(
+    el,
+    document.getElementById('root')
+);
