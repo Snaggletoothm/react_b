c6b66923240fa2f0fa32c41cab1077c47a668bf9
Mikriukov MA <snagglet00th@yandex.ru>
2022-03-12 14:21:55 +0500

    Naive rendering several items without loop

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -2,11 +2,19 @@ import React from "react";
 import TodoListItem from "./todo-list-item";
 
 
-const TodoList = () => {
+const TodoList = ({todos}) => {
     return (
         <ul>
-            <li><TodoListItem label='drink tea' /></li>
-            <li><TodoListItem label='eat meat' important /></li>
+            <li>
+                <TodoListItem 
+                    label={todos[0].label} 
+                    important={todos[0].important} />
+            </li>
+            <li>
+                <TodoListItem 
+                    label={todos[1].label} 
+                    important={todos[1].important} />
+            </li>
         </ul>
     );
 };
--- src/index.js
+++ src/index.js
@@ -7,12 +7,18 @@ import TodoList from './components/todo-list';
 
 
 const App = () => {
+    const todoData = [
+        {'label': 'drink milk', 'important': false},
+        {'label': 'study data structure and algorithms', 'important': true},
+        {'label': 'have a lunch', 'important': false},
+    ];
+
     return (
         <div>
             <span>{(new Date()).toString()}</span>
             <AppHeader />        
             <SearchPanel />
-            <TodoList />
+            <TodoList todos={todoData}/>
         </div>
     );
 };
