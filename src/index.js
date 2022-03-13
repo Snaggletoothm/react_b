ed7ea901a525de154e660dc2aba3df4d83ba456b
Mikriukov MA <snagglet00th@yandex.ru>
2022-03-13 13:27:12 +0500

    create dirs for all components and add styles

--- src/index.js
+++ src/index.js
@@ -1,27 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import TodoList from './components/todo-list';
-
-
-const App = () => {
-    const todoData = [
-        {label: 'drink milk', important: false, id: 1},
-        {label: 'study data structure and algorithms', important: true, id: 2},
-        {label: 'have a lunch', important: false, id: 3},
-    ];
-
-    return (
-        <div>
-            <span>{(new Date()).toString()}</span>
-            <AppHeader />        
-            <SearchPanel />
-            <TodoList todos={todoData}/>
-        </div>
-    );
-};
+import App from './components/app'
 
 
 ReactDOM.render(
