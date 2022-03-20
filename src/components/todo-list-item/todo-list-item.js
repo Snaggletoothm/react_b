6a4d6f0b52c2731812232f15768b28d444914c59
Mikriukov MA <snagglet00th@yandex.ru>
2022-03-20 21:45:21 +0500

    add behavior @done@ upon click on item

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -5,10 +5,23 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
-  onLabelClick = () => console.log(`lets to it >> ${this.props.label}`);
+  state = {
+    done: false,
+  };
+
+  onLabelClick = () => this.setState({done: true});
+  
 
   render() {
     const {label, important=false} = this.props;
+
+    const {done} = this.state;
+
+    let classNames = 'todo-list-item';
+
+    if(done) {
+      classNames += ' done';
+    }
 
     const style = {
       color: important ? 'steelblue' : 'black',
@@ -16,7 +29,7 @@ export default class TodoListItem extends Component {
     };
   
     return (
-      <span className="todo-list-item">
+      <span className={classNames}>
         <span
           className="todo-list-item-label"
           style={style}
