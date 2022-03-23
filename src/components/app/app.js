624d793e77ca82e34c51039088a1e28b7838b4df
Mikriukov MA <snagglet00th@yandex.ru>
2022-03-23 13:01:08 +0500

    refactor: mv func to class component @App@

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
@@ -7,25 +7,29 @@ import ItemStatusFilter from '../item-status-filter';
 
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
 
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
-  ];
+  state = {
+    todoData: [
+      { label: 'Drink Coffee', important: false, id: 1 },
+      { label: 'Make Awesome App', important: true, id: 2 },
+      { label: 'Have a lunch', important: false, id: 3 }
+    ]
+  };
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={4} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+  deleteItem = (id) => console.log(`delete id=${id}`);
+
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={4} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+  
+        <TodoList todos={this.state.todoData} onDel={this.deleteItem} />
       </div>
-
-      <TodoList todos={todoData} onDel={(id) => console.log(`del id=${id}`)} />
-    </div>
-  );
+    );
+  }  
 };
-
-export default App;
