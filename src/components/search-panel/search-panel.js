b1e4c4f08f9e7d1ef7414077ed6243a0d00b70f7
Mikriukov MA <snagglet00th@yandex.ru>
2022-03-27 14:11:26 +0500

    implementation of filtering by label item

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './search-panel.css';
 
-const SearchPanel = () => {
-  return (
-    <input type="text"
-              className="form-control search-input"
-              placeholder="type to search" />
-  );
+class SearchPanel extends Component {
+
+  onSearchInputChange = e => {
+    this.props.onFilterItems(e.target.value);
+  };
+
+  render() {
+    return (
+      <input type="text"
+                className="form-control search-input"
+                onChange={this.onSearchInputChange}
+                placeholder="type to search" 
+      />
+    );
+  }
 };
 
 export default SearchPanel;
